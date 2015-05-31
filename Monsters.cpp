91a42bfcb2ca7eaad72c7da45822c6b65d0161d0
Jglamour <jasdeepglamour@gmail.com>
2015-05-31 18:13:52 -0400

    Update Monsters.cpp

--- Monsters.cpp
+++ Monsters.cpp
@@ -1,1 +1,19 @@
 
+#include string;
+using namespace std;
+
+#include Monsters.h
+
+IntroDisplay()
+{
+  string intro="There is a fifth dimension beyond that which is known to man. It is a dimension as vast as space, and as timeless as infinity. It is the middle ground between light and shadow - between science and superstition. And it lies between the pit of man's fears and the summit of his knowledge. This is the dimension of imagination. It is an area which we call the Twilight Zone.";
+  string mapleInto="Which reads, "Maple Street." PAN DOWN until we are shooting down at an angle toward the street below. It's a tree-lined, quiet residential American street, very typical of the small town. The houses have front porches on which people sit and swing on gliders, conversing across from house to house. Steve Brand polishes his car parked in front of his house. His neighbor, Don Martin, leans against the fender watching him. A Good Humor man rides a bicycle and is just in the process of stopping to sell some ice cream to a couple of kids. Two women gossip on the front lawn. Another man waters his lawn. Maple Street, U.S.A., late summer. A tree-lined little world of front porch gliders, hop scotch, the laughter of children, and the bell of an ice cream vendor.";
+  
+  cout<<intro<<endl<<mapleIntro<<endl;
+}
+
+Meteor()
+{
+  string meteorConvo="At the sound of the roar and the flash of light it will be precisely 6:43 P.M. on Maple Street. At this moment one of the little boys, Tommy, looks up to listen to a sound of a tremendous screeching roar from overhead. A flash of light plays on both their faces and then it moves down the street past lawns and porches and rooftops and then disappears.\n";
+  
+}
