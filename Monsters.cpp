0c742eb8abc113be373f8c5790dba922110f48df
Jglamour <jasdeepglamour@gmail.com>
2015-05-31 18:59:35 -0400

    Update Monsters.cpp

--- Monsters.cpp
+++ Monsters.cpp
@@ -15,5 +15,14 @@ IntroDisplay()
 Meteor()
 {
   string meteorConvo="At the sound of the roar and the flash of light it will be precisely 6:43 P.M. on Maple Street. At this moment one of the little boys, Tommy, looks up to listen to a sound of a tremendous screeching roar from overhead. A flash of light plays on both their faces and then it moves down the street past lawns and porches and rooftops and then disappears.\n";
+  string steveDonMeteor1="STEVE: What was that? A meteor?/n DON: That's what it looked like. I didn't hear an crash, though, did you?\n STEVE: Nope. I didn't hear anything except a roar.";
+  string SteveMrsMeteor="MRS.BRAND:(from her porch)Steve? What was that?\nSTEVE:(raising his voice and looking toward porch) Guess it was a meteor, honey. Came awful close, didn't it?\nMRS.BRAND:"Too close for my money! Much too close.\n"
+  
+  string meteorSetting="Ext. Street Day Slow pan across the porches again Interspersed with MED. CLOSE SHOT, man screwing a light bulb on a front porch, then getting down off the stool to flick the switch and finding that nothing happens.\n Med. shot Man:Working on an electric power mower. He plugs in the plug, flicks on the switch of the power mower, off and on, with nothing happening.\nMed. close shot through window of a front porch Woman on a porch:Pushing her finger back and forth on the dial hook. Her voice is indistinct and distant, but intelligible and repetitive.\nWoman Next Door:Operator, operator, something\'s wrong on the phone, operator!\nMed. close shot Mrs. Brand As she comes out on the porch and calls to Steve.\n"
+  
+  string electricityMeteor="Long angle shot looking down the street\nAs we hear the voices creep up from down below, small, mildly disturbed voices, highlighting these kinds of phrases:\nElectricity's off.\nPhone won't work.\nCan't get a thing on the radio.\nMy power motor won't move,\nwon't work at all.\nRadio's gone dead.\n";
+  
+  string MeteorSetting2="While out on the curb you see Pete Van Horn  A tall, thin man who is standing in front of his house.\nVan Horn:I'll cut through the backyard...See if the power's still on on Floral Street. I'll be right back.\n Long shot Van Horn:As he walks past the side of his house and disappears into the backyard. You look at the hammer on his belt.\nUntil we're looking at ten or eleven people standing around the street and overflowing to the curb and sidewalk. In the background is Steve Brand's car.";
+  
   
 }
