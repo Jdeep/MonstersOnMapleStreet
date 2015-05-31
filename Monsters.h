dbfac80551b50fff82916c699e0d0b95ee569081
Jglamour <jasdeepglamour@gmail.com>
2015-05-31 17:36:38 -0400

    Create Monsters.h

---
+++ Monsters.h
@@ -0,0 +1,29 @@
+//----------------------------------------------------------------------------
+//
+//  Monsters on Maple Street Header
+//   This Program is based on the Monsters on Maple street Epsoide. 
+//
+//  
+//
+//----------------------------------------------------------------------------
+//
+//  Author:         Justin Ickler, Jasdeep Glamour
+//  Course:        	English 103
+//  Date:           05/31/2015
+
+//----------------------------------------------------------------------------
+
+#ifndef _Monsters_h_
+#define _Monsters_h_
+
+#include <string>
+using namespace std ;
+
+Class Monsters
+{
+	public:
+		
+
+
+
+}
